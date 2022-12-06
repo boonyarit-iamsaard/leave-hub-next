1da0bff48a82aaf59c445b821f5be8c8d727c1a6
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2022-12-06 18:11:08 +0700

    refactor: reorganize imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import styles from './index.module.css';
 import { type NextPage } from 'next';
+import { signIn, signOut, useSession } from 'next-auth/react';
 import Head from 'next/head';
 import Link from 'next/link';
-import { signIn, signOut, useSession } from 'next-auth/react';
 
 import { trpc } from '../utils/trpc';
+import styles from './index.module.css';
 
 const Home: NextPage = () => {
   const hello = trpc.example.hello.useQuery({ text: 'from tRPC' });
