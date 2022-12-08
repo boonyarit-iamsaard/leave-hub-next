83d4ea55f7c17b0ae1333f56c19d0c7d24310e00
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2022-12-08 17:46:43 +0700

    build: implement roster nav link

--- src/components/layout/LayoutNavLinkList.tsx
+++ src/components/layout/LayoutNavLinkList.tsx
@@ -1,46 +1,72 @@
 import type { Dispatch, FC, SetStateAction } from 'react';
 
 import { NavLink } from '@mantine/core';
+import { Role } from '@prisma/client';
+import dayjs from 'dayjs';
+import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-
-import { MdOutlineHome, MdOutlineManageAccounts } from 'react-icons/md';
+import {
+  MdOutlineCalendarViewMonth,
+  MdOutlineHome,
+  MdOutlineManageAccounts,
+} from 'react-icons/md';
 
 interface LayoutNavLinkListProps {
   setOpen: Dispatch<SetStateAction<boolean>>;
 }
 
-const linkList = [
-  {
-    icon: MdOutlineHome,
-    label: 'Home',
-    pathname: '/',
-  },
-  {
-    icon: MdOutlineManageAccounts,
-    label: 'Admin',
-    pathname: '/admin',
-  },
-];
-
 const LayoutNavLinkList: FC<LayoutNavLinkListProps> = ({ setOpen }) => {
   const { pathname } = useRouter();
+  const { data: sessionData } = useSession();
 
-  // TODO: Implement protected admin route
+  const currentYear = dayjs().year();
+  const currentMonth = dayjs().month() + 1;
 
-  const items = linkList.map(link => (
-    <NavLink
-      active={link.pathname === pathname}
-      component={Link}
-      href={link.pathname}
-      icon={<link.icon size={16} />}
-      key={link.label}
-      label={link.label}
-      onClick={() => setOpen(false)}
-    />
-  ));
+  const getDefaultRosterPath = (roster: 'mechanic' | 'engineer') => {
+    return `/roster/${roster}/${currentYear}/${currentMonth}`;
+  };
 
-  return <>{items}</>;
+  return (
+    <>
+      <NavLink
+        active={pathname === '/'}
+        component={Link}
+        href="/"
+        icon={<MdOutlineHome size={16} />}
+        label="Home"
+        onClick={() => setOpen(false)}
+      />
+      <NavLink
+        active={pathname.includes('/roster')}
+        label="Roster"
+        icon={<MdOutlineCalendarViewMonth size={16} />}
+      >
+        <NavLink
+          href={getDefaultRosterPath('engineer')}
+          label="Engineer"
+          component={Link}
+          onClick={() => setOpen(false)}
+        />
+        <NavLink
+          label="Mechanic"
+          href={getDefaultRosterPath('mechanic')}
+          component={Link}
+          onClick={() => setOpen(false)}
+        />
+      </NavLink>
+      {sessionData?.user?.role === Role.ADMIN && (
+        <NavLink
+          active={pathname.includes('/admin')}
+          component={Link}
+          href="/admin"
+          icon={<MdOutlineManageAccounts size={16} />}
+          label="Admin"
+          onClick={() => setOpen(false)}
+        />
+      )}
+    </>
+  );
 };
 
 export default LayoutNavLinkList;
