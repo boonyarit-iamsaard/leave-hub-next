5c522504b476189d796369296dbe9127e383087b
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2022-12-09 23:02:44 +0700

    refactor: wrap seed script in try/catch

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -18,68 +18,68 @@ import users from '../seed-data/users.json';
 const prisma = new PrismaClient();
 
 const main = async () => {
-  publicHolidays.forEach(async holiday => {
-    const { date, name } = holiday;
-    await prisma.publicHoliday.upsert({
-      where: { date: new Date(date) },
-      update: {},
-      create: {
-        date: new Date(date),
-        name,
-      },
-    });
-
-    console.log(`Public holiday ${name} created successfully ✅`);
-  });
-  users.forEach(async user => {
-    const { name, username, password, email, role, roster } = user;
-
-    const hashedPassword = await hash(password);
-    const userRole = role === 'ADMIN' ? Role.ADMIN : Role.USER;
-    const userRoster =
-      roster === 'ENGINEER' ? Roster.ENGINEER : Roster.MECHANIC;
-    const userEntitlements = entitlements
-      .filter(entitlement => entitlement.username === username)
-      .map(({ year, amount, name }) => ({ year, amount, name }));
-    const userShifts = shifts
-      .filter(shift => shift.username === username)
-      .map(({ type, status, priority, start, end }) => ({
-        type: ShiftType[type as keyof typeof ShiftType],
-        status: ShiftStatus[status as keyof typeof ShiftStatus],
-        priority: ShiftPriority[priority as keyof typeof ShiftPriority],
-        start: new Date(start),
-        end: new Date(end),
-        amount: dayjs(end).diff(dayjs(start), 'day') + 1,
-      }));
-    const userRosterSequence = rosterSequence
-      .filter(sequence => sequence.username === username)
-      .map(({ year, sequence }) => ({ year, sequence }));
-
-    await prisma.user.upsert({
-      where: { username: user.username },
-      update: {},
-      create: {
-        name,
-        username,
-        email,
-        role: userRole,
-        roster: userRoster,
-        password: hashedPassword,
-        entitlements: {
-          create: userEntitlements,
-        },
-        shifts: {
-          create: userShifts,
-        },
-        rosterSequence: {
-          create: userRosterSequence,
-        },
-      },
-    });
-
-    console.log(`User ${user.username} created successfully ✅`);
-  });
   try {
+    publicHolidays.forEach(async holiday => {
+      const { date, name } = holiday;
+      await prisma.publicHoliday.upsert({
+        where: { date: new Date(date) },
+        update: {},
+        create: {
+          date: new Date(date),
+          name,
+        },
+      });
+
+      console.log(`Public holiday ${name} created successfully ✅`);
+    });
+    users.forEach(async user => {
+      const { name, username, password, email, role, roster } = user;
+
+      const hashedPassword = await hash(password);
+      const userRole = role === 'ADMIN' ? Role.ADMIN : Role.USER;
+      const userRoster =
+        roster === 'ENGINEER' ? Roster.ENGINEER : Roster.MECHANIC;
+      const userEntitlements = entitlements
+        .filter(entitlement => entitlement.username === username)
+        .map(({ year, amount, name }) => ({ year, amount, name }));
+      const userShifts = shifts
+        .filter(shift => shift.username === username)
+        .map(({ type, status, priority, start, end }) => ({
+          type: ShiftType[type as keyof typeof ShiftType],
+          status: ShiftStatus[status as keyof typeof ShiftStatus],
+          priority: ShiftPriority[priority as keyof typeof ShiftPriority],
+          start: new Date(start),
+          end: new Date(end),
+          amount: dayjs(end).diff(dayjs(start), 'day') + 1,
+        }));
+      const userRosterSequence = rosterSequence
+        .filter(sequence => sequence.username === username)
+        .map(({ year, sequence }) => ({ year, sequence }));
+
+      await prisma.user.upsert({
+        where: { username: user.username },
+        update: {},
+        create: {
+          name,
+          username,
+          email,
+          role: userRole,
+          roster: userRoster,
+          password: hashedPassword,
+          entitlements: {
+            create: userEntitlements,
+          },
+          shifts: {
+            create: userShifts,
+          },
+          rosterSequence: {
+            create: userRosterSequence,
+          },
+        },
+      });
+
+      console.log(`User ${user.username} created successfully ✅`);
+    });
   } catch (error) {
     console.log('⛔ Error seeding database: ', error);
     process.exit(1);
